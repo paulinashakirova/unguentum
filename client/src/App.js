574ceb3c310e3382fd7ef37ef3cf867825242819
PaulinaShakirova <pollisha.jeans@gmail.com>
2021-06-15 12:26:41 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,6 @@
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+export default function App() {
+  return <div className='App'></div>;
+}
---
+++ model/perfumes.sql
@@ -0,0 +1,16 @@
+
+DROP TABLE IF EXISTS `perfumes`;
+
+
+CREATE TABLE `perfumes` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`brand` varchar(255) NOT NULL,
+	`scent` varchar(255) NOT NULL,
+	`style` varchar(255) NOT NULL,
+	`season` varchar(255) NOT NULL,
+	`gender` varchar(255) NOT NULL,
+	`time_of_day` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
